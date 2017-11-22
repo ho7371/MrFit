8c7117f9bbf8e316a67c089eba3fa58ef5825f5d
kosta <kosta@192.168.0.179>
2017-11-22 16:04:12 +0900

    [진호][11/22][16:03] 파일업로드 클래스 준비

--- MrFit/src/main/java/org/kosta/MrFit/controller/AdminController.java
+++ MrFit/src/main/java/org/kosta/MrFit/controller/AdminController.java
@@ -1,5 +1,7 @@
 package org.kosta.MrFit.controller;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.Resource;
@@ -8,10 +10,14 @@ import javax.servlet.http.HttpServletRequest;
 import org.kosta.MrFit.model.PagingBean;
 import org.kosta.MrFit.model.ProductService;
 import org.kosta.MrFit.model.ProductVO;
+import org.kosta.MrFit.model.UploadVO;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AdminController {
@@ -55,6 +61,38 @@ public class AdminController {
 		System.out.println("   	AdminController/registerProduct()/시작");
 		return "admin/registerProductForm.tiles";
 	}
+	
+	
+	private String uploadPath;
+	@Secured("ROLE_ADMIN")
+	@RequestMapping("admin/registerProduct.do")
+	public ModelAndView registerProduct(UploadVO vo, HttpServletRequest request) {
+		System.out.println("   	AdminController/registerProduct()/시작");
+		uploadPath=request.getSession().getServletContext().getRealPath("/resources/upload");
+		File uploadDir=new File(uploadPath);
+		if(uploadDir.exists()==false) {
+			uploadDir.mkdirs();
+		}
+		List<MultipartFile> list=vo.getFiles();
+		//view 화면에 업로드 된 파일 목록을 전달하기 위한 리스트 
+		ArrayList<String> nameList=new ArrayList<String>();
+		for(int i=0;i<list.size();i++){
+			//만약 업로드 파일이 없으면 파일명은 공란처리된다 
+			String fileName=list.get(i).getOriginalFilename();
+			//업로드 파일이 있으면 파일을 특정경로로 업로드한다
+			if(!fileName.equals("")){			
+				try {
+					list.get(i).transferTo(new File(uploadPath+fileName));
+					System.out.println(fileName+" 업로드 완료");
+					nameList.add(fileName);
+				} catch (Exception e) {					
+					e.printStackTrace();
+				} 
+			}
+		}
+		return new ModelAndView("product/multifileupload_result.tiles","nameList",nameList);
+	}
+	
 	
 	@Secured("ROLE_ADMIN")
 	@RequestMapping("admin/updateProductForm.do")
