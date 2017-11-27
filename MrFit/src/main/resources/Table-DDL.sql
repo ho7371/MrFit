a616eccf3bfc2bf364e2044b4dfc6ee627022f80
kosta <kosta@192.168.0.113>
2017-11-27 18:17:14 +0900

    [석환][11.27][18:16] 포인트 적립 수정

--- MrFit/src/main/resources/Table-DDL.sql
+++ MrFit/src/main/resources/Table-DDL.sql
@@ -1,5 +1,5 @@
 ----------------------------------------------------- 삭제 SQL
------------- 삭제순서 : 1. 시퀀스 삭제  		2. 테이블 삭제
+------------ 삭제순서 : 1. 시퀀스 삭제        2. 테이블 삭제
 ------------ 시퀀스 삭제
 drop sequence note_no_seq;
 drop table note;
@@ -34,12 +34,12 @@ drop TABLE member;
 drop sequence qno_seq;
 drop TABLE question;
 drop TABLE grade;
-	
+   
 ----------------------------------------------------- 삭제 SQL 종료
 
 -------------------------------------------------------- 테이블 생성 SQL
 
------------- 생성순서 : 1. 시퀀스 생성  		2. 테이블 생성
+------------ 생성순서 : 1. 시퀀스 생성        2. 테이블 생성
 
 ------------ 외래키 이름은 일단 지어놨고, 규칙 정해서 나중에 확정할 것 : 진호
 
@@ -48,162 +48,162 @@ drop TABLE grade;
 
 /* 회원 등급 */
 CREATE TABLE grade (
-	grade VARCHAR2(100) PRIMARY KEY, 
-	percent NUMBER NOT NULL 
+   grade VARCHAR2(100) PRIMARY KEY, 
+   percent NUMBER NOT NULL 
 );
 
 /* 비밀번호 찾기 질문 */  
 create sequence qno_seq;
 CREATE TABLE question (
-	qno NUMBER PRIMARY KEY, 
-	question VARCHAR2(100) NOT NULL 
+   qno NUMBER PRIMARY KEY, 
+   question VARCHAR2(100) NOT NULL 
 );
 
 /* 회원 */
 CREATE TABLE member (
-	id VARCHAR2(100) PRIMARY KEY,
-	password VARCHAR2(100) NOT NULL, 
-	name VARCHAR2(100) NOT NULL, 
-	phone VARCHAR2(100) NOT NULL,
-	address VARCHAR2(100) NOT NULL, 
-	email VARCHAR2(100) NOT NULL,
-	point NUMBER default 0, 
-	totalspent NUMBER default 0,
-	status VARCHAR2(100) default 1, 
-	answer VARCHAR2(100) NOT NULL, 
-	qno NUMBER NOT NULL, 
-	grade VARCHAR2(100) default '브론즈',
-	constraint fk_qno_in_member foreign key(qno) references question(qno),
-	constraint fk_grade_in_member foreign key(grade) references grade(grade)
+   id VARCHAR2(100) PRIMARY KEY,
+   password VARCHAR2(100) NOT NULL, 
+   name VARCHAR2(100) NOT NULL, 
+   phone VARCHAR2(100) NOT NULL,
+   address VARCHAR2(100) NOT NULL, 
+   email VARCHAR2(100) NOT NULL,
+   point NUMBER default 0, 
+   totalspent NUMBER default 0,
+   status VARCHAR2(100) default 1, 
+   answer VARCHAR2(100) NOT NULL, 
+   qno NUMBER NOT NULL, 
+   grade VARCHAR2(100) default '브론즈',
+   constraint fk_qno_in_member foreign key(qno) references question(qno),
+   constraint fk_grade_in_member foreign key(grade) references grade(grade)
 );
 
-		/* 회원치수 */
-		CREATE TABLE member_size (
-			id VARCHAR2(100) NOT NULL,
-			shoulder NUMBER, 
-			chest NUMBER, 
-			sleeve NUMBER, 
-			armhole NUMBER,
-			toplength NUMBER, 
-			waist NUMBER,
-			crotch NUMBER, 
-			thigh NUMBER, 
-			hem NUMBER, 
-			bottomlength NUMBER,
-			constraint fk_id_in_member foreign key(id) references member(id)
-		);
-		CREATE INDEX member_size_unique ON member_size(id);
+      /* 회원치수 */
+      CREATE TABLE member_size (
+         id VARCHAR2(100) NOT NULL,
+         shoulder NUMBER, 
+         chest NUMBER, 
+         sleeve NUMBER, 
+         armhole NUMBER,
+         toplength NUMBER, 
+         waist NUMBER,
+         crotch NUMBER, 
+         thigh NUMBER, 
+         hem NUMBER, 
+         bottomlength NUMBER,
+         constraint fk_id_in_member foreign key(id) references member(id)
+      );
+      CREATE INDEX member_size_unique ON member_size(id);
 
 
 /* 포인트 */
 create sequence point_no_seq;
 CREATE TABLE point (
-	point_no NUMBER PRIMARY KEY, 
-	id VARCHAR2(100) NOT NULL, 
-	updown NUMBER NOT NULL,
-	change_date date NOT NULL,
-	constraint fk_id_in_point foreign key(id) references member(id)
+   point_no NUMBER PRIMARY KEY, 
+   id VARCHAR2(100) NOT NULL, 
+   updown NUMBER NOT NULL,
+   change_date date NOT NULL,
+   constraint fk_id_in_point foreign key(id) references member(id)
 );
 
 /* 권한 */
 CREATE TABLE auth (
-	id VARCHAR2(100) NOT NULL,
-	auth VARCHAR2(100) NOT NULL,
-	constraint fk_id_in_auth foreign key(id) references member(id),
-	constraint pk_auth primary key(id, auth)
+   id VARCHAR2(100) NOT NULL,
+   auth VARCHAR2(100) NOT NULL,
+   constraint fk_id_in_auth foreign key(id) references member(id),
+   constraint pk_auth primary key(id, auth)
 );
 
 
 /* 상품 */
 create sequence pno_seq;
 CREATE TABLE product (
-	pno NUMBER PRIMARY KEY, 
-	name VARCHAR2(100) NOT NULL,
-	price NUMBER NOT NULL,
-	content CLOB NOT NULL, 
-	category VARCHAR2(100) NOT NULL
+   pno NUMBER PRIMARY KEY, 
+   name VARCHAR2(100) NOT NULL,
+   price NUMBER NOT NULL,
+   content CLOB NOT NULL, 
+   category VARCHAR2(100) NOT NULL
 );
 
-		/* 상품 색상 [진행중인 테이블 선택시 추가해야 할 테이블] */	
-		create sequence pcno_seq;
-		CREATE TABLE product_color (
-			pcno NUMBER PRIMARY KEY,
-			color_name VARCHAR2(100) NOT NULL
-		);
-			
-		/* 상품치수 [작업중인 테이블]*/
-		create sequence psno_seq;
-		CREATE TABLE product_size (
-			psno NUMBER PRIMARY KEY,
-			size_name VARCHAR2(100) NOT NULL, /* 칼럼명 변경 */
-			size1 NUMBER NOT NULL,
-			size2 NUMBER NOT NULL,
-			size3 NUMBER NOT NULL,
-			size4 NUMBER NOT NULL,
-			size5 NUMBER NOT NULL
-		);
-		
-		/* 상품상세 [작업중인 테이블]*/
-		create sequence pdno_seq;
-		CREATE TABLE product_detail (
-			pdno NUMBER PRIMARY KEY,
-			pno NUMBER NOT NULL,
-			pcno NUMBER NOT NULL, 
-			psno NUMBER NOT NULL,
-			inventory NUMBER NOT NULL,
-			constraint fk_pno_in_product_detail foreign key(pno) references product(pno) on delete cascade,
-			constraint fk_color_in_product_detail foreign key(pcno) references product_color(pcno) on delete cascade,
-			constraint fk_psno_in_product_detail foreign key(psno) references product_size(psno) on delete cascade
-		);
-			CREATE INDEX product_detail_unique ON product_detail(pno, pcno, psno);
-			/* CREATE INDEX 인덱스명 ON 테이블명(칼럼1, 칼럼2, 칼럼3); */
+      /* 상품 색상 [진행중인 테이블 선택시 추가해야 할 테이블] */   
+      create sequence pcno_seq;
+      CREATE TABLE product_color (
+         pcno NUMBER PRIMARY KEY,
+         color_name VARCHAR2(100) NOT NULL
+      );
+         
+      /* 상품치수 [작업중인 테이블]*/
+      create sequence psno_seq;
+      CREATE TABLE product_size (
+         psno NUMBER PRIMARY KEY,
+         size_name VARCHAR2(100) NOT NULL, /* 칼럼명 변경 */
+         size1 NUMBER NOT NULL,
+         size2 NUMBER NOT NULL,
+         size3 NUMBER NOT NULL,
+         size4 NUMBER NOT NULL,
+         size5 NUMBER NOT NULL
+      );
+      
+      /* 상품상세 [작업중인 테이블]*/
+      create sequence pdno_seq;
+      CREATE TABLE product_detail (
+         pdno NUMBER PRIMARY KEY,
+         pno NUMBER NOT NULL,
+         pcno NUMBER NOT NULL, 
+         psno NUMBER NOT NULL,
+         inventory NUMBER NOT NULL,
+         constraint fk_pno_in_product_detail foreign key(pno) references product(pno) on delete cascade,
+         constraint fk_color_in_product_detail foreign key(pcno) references product_color(pcno) on delete cascade,
+         constraint fk_psno_in_product_detail foreign key(psno) references product_size(psno) on delete cascade
+      );
+         CREATE INDEX product_detail_unique ON product_detail(pno, pcno, psno);
+         /* CREATE INDEX 인덱스명 ON 테이블명(칼럼1, 칼럼2, 칼럼3); */
 
 
 /* 고객문의 게시판 */
 create sequence iqno_seq;
 CREATE TABLE inquiry (
-	iqno NUMBER PRIMARY KEY, 
-	content CLOB NOT NULL, 
-	regdate DATE NOT NULL, 
-	security VARCHAR2(100) NOT NULL, 
-	id VARCHAR2(100) NOT NULL,
-	constraint fk_id_in_inquiry foreign key(id) references member(id)
+   iqno NUMBER PRIMARY KEY, 
+   content CLOB NOT NULL, 
+   regdate DATE NOT NULL, 
+   security VARCHAR2(100) NOT NULL, 
+   id VARCHAR2(100) NOT NULL,
+   constraint fk_id_in_inquiry foreign key(id) references member(id)
 );
 
-		/* 고객문의 댓글 */
-		create sequence iqrno_seq;
-		CREATE TABLE inquiry_reply (
-			iqrno NUMBER PRIMARY KEY,
-			content CLOB NOT NULL,
-			regdate DATE NOT NULL, 
-			security VARCHAR2(100) NOT NULL, 
-			iqno NUMBER NOT NULL,
-			constraint fk_iqno_in_inquiry_reply foreign key(iqno) references inquiry(iqno)
-		);
+      /* 고객문의 댓글 */
+      create sequence iqrno_seq;
+      CREATE TABLE inquiry_reply (
+         iqrno NUMBER PRIMARY KEY,
+         content CLOB NOT NULL,
+         regdate DATE NOT NULL, 
+         security VARCHAR2(100) NOT NULL, 
+         iqno NUMBER NOT NULL,
+         constraint fk_iqno_in_inquiry_reply foreign key(iqno) references inquiry(iqno)
+      );
 
 /* 리뷰 */
 create sequence rno_seq;
 CREATE TABLE review (
-	rno NUMBER PRIMARY KEY,
-	pdno NUMBER NOT NULL,
-	id VARCHAR2(100) NOT NULL,
-	content CLOB NOT NULL,
-	regdate DATE NOT NULL,
-	constraint fk_pdno_in_review foreign key(pdno) references product_detail(pdno),
-	constraint fk_id_in_review foreign key(id) references member(id)
+   rno NUMBER PRIMARY KEY,
+   pdno NUMBER NOT NULL,
+   id VARCHAR2(100) NOT NULL,
+   content CLOB NOT NULL,
+   regdate DATE NOT NULL,
+   constraint fk_pdno_in_review foreign key(pdno) references product_detail(pdno),
+   constraint fk_id_in_review foreign key(id) references member(id)
 );
 CREATE INDEX review_unique ON review(pdno,id);
 
 /* 상품 문의 */
 create sequence pqno_seq;
 CREATE TABLE product_qna (
-	pqno NUMBER PRIMARY KEY,
-	id VARCHAR2(100) NOT NULL,
-	pno NUMBER NOT NULL,
-	content CLOB NOT NULL,
-	regdate DATE NOT NULL,
-	constraint fk_id_in_product_qna foreign key(id) references member(id),
-	constraint fk_pno_in_product_qna foreign key(pno) references product(pno) on delete cascade
+   pqno NUMBER PRIMARY KEY,
+   id VARCHAR2(100) NOT NULL,
+   pno NUMBER NOT NULL,
+   content CLOB NOT NULL,
+   regdate DATE NOT NULL,
+   constraint fk_id_in_product_qna foreign key(id) references member(id),
+   constraint fk_pno_in_product_qna foreign key(pno) references product(pno) on delete cascade
 );
 
 
@@ -211,57 +211,57 @@ CREATE TABLE product_qna (
 
 create sequence ino_seq;
 CREATE TABLE image (
-	ino NUMBER PRIMARY KEY, 
-	pno NUMBER NOT NULL, 
-	url VARCHAR2(300) NOT NULL,
-	constraint fk_pno_in_image foreign key(pno) references product(pno) on delete cascade
+   ino NUMBER PRIMARY KEY, 
+   pno NUMBER NOT NULL, 
+   url VARCHAR2(300) NOT NULL,
+   constraint fk_pno_in_image foreign key(pno) references product(pno) on delete cascade
 );
 
 /* 주문 */
 create sequence ono_seq;
 CREATE TABLE orders (
-	ono NUMBER PRIMARY KEY, 
-	id VARCHAR2(100) NOT NULL, 
-	totalprice NUMBER NOT NULL, 
-	ordertime DATE NOT NULL,
-	status VARCHAR2(100) NOT NULL,
-	destination VARCHAR2(100) NOT NULL,
-	constraint fk_id_in_orders foreign key(id) references member(id)
+   ono NUMBER PRIMARY KEY, 
+   id VARCHAR2(100) NOT NULL, 
+   totalprice NUMBER NOT NULL, 
+   ordertime DATE NOT NULL,
+   status VARCHAR2(100) NOT NULL,
+   destination VARCHAR2(100) NOT NULL,
+   constraint fk_id_in_orders foreign key(id) references member(id)
 );
 
 /* 주문상품 */
 CREATE TABLE order_product (
-	ono NUMBER NOT NULL, 
-	pdno NUMBER NOT NULL,
-	quantity VARCHAR2(100) NOT NULL,
-	constraint fk_ono_in_product_detail foreign key(ono) references orders(ono),
-	constraint fk_pdno_in_product_detail foreign key(pdno) references product_detail(pdno),
-	constraint pk_order_product primary key(ono, pdno)
+   ono NUMBER NOT NULL, 
+   pdno NUMBER NOT NULL,
+   quantity VARCHAR2(100) NOT NULL,
+   constraint fk_ono_in_product_detail foreign key(ono) references orders(ono),
+   constraint fk_pdno_in_product_detail foreign key(pdno) references product_detail(pdno),
+   constraint pk_order_product primary key(ono, pdno)
 );
 
-/* 게시판 */		
+/* 게시판 */      
 create sequence bno_seq;
 CREATE TABLE board (
-	bno NUMBER PRIMARY KEY, 
-	title VARCHAR2(100) NOT NULL, 
-	id VARCHAR2(100) NOT NULL,
-	content CLOB NOT NULL,
-	regdate DATE NOT NULL,
-	security VARCHAR2(100) NOT NULL,
-	category VARCHAR2(100) NOT NULL,
-	constraint fk_id_in_board foreign key(id) references member(id)
+   bno NUMBER PRIMARY KEY, 
+   title VARCHAR2(100) NOT NULL, 
+   id VARCHAR2(100) NOT NULL,
+   content CLOB NOT NULL,
+   regdate DATE NOT NULL,
+   security VARCHAR2(100) NOT NULL,
+   category VARCHAR2(100) NOT NULL,
+   constraint fk_id_in_board foreign key(id) references member(id)
 );
 
 /* 쪽지 */
 create sequence note_no_seq;
 CREATE TABLE note(
-	note_no NUMBER PRIMARY KEY,
-	content CLOB NOT NULL,
-	send_date DATE NOT NULL,
-	id VARCHAR2(100) NOT NULL,
-	constraint fk_id_in_note foreign key(id) references member(id)
+   note_no NUMBER PRIMARY KEY,
+   content CLOB NOT NULL,
+   send_date DATE NOT NULL,
+   id VARCHAR2(100) NOT NULL,
+   constraint fk_id_in_note foreign key(id) references member(id)
 );
-				
+            
 ----------------------------------------------------- 테이블 생성 SQL 끝
 
 ----------------------------------------------------- 테이블 조회 SQL
